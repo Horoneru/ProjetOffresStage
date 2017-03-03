8dbf1b3992bbba7283ff899d841f2815bdcbc78d
Horoneru <horonerudev@gmail.com>
2017-03-03 17:38:18 +0100

    Proper documentation

--- src/fr/cils/projet/stage/dao/Dao.java
+++ src/fr/cils/projet/stage/dao/Dao.java
@@ -6,10 +6,34 @@ import java.sql.Connection;
 
 public abstract class Dao<T>
 {
-    public Connection connect = Db.getInstance();
 
-    public abstract T find(int id); //Permet de récupérer un objet via son ID
-    public abstract T create(T obj); //Permet de créer une entrée dans la base de données par rapport à un objet
-    public abstract T update(T obj); //Permet de mettre à jour les données d'une entrée dans la base
-    public abstract void delete(T obj); //Permet la suppression d'une entrée de la base
+    //Liaison à la db. Seules les sous classes de Dao<T> l'utilise
+    //Package-private
+    Connection connect = Db.getInstance();
+
+    /**
+     * Cherche un objet T avec un identifiant id.
+     * @param id l'identifiant de l'objet T cherché
+     * @return T l'objet T ou null si celui-ci n'est pas trouvé ou si une erreur est survenue
+     */
+    public abstract T find(int id);
+
+    /**
+     * Insère l'objet obj dans la db
+     * @param obj l'objet que l'on souhaite persister
+     * @return T l'objet T inséré dans la db ou null si celui-ci n'est pas trouvé
+     */
+    public abstract T create(T obj);
+
+    /**
+     * UNIMPLEMENTED (not in specs)
+     * @return null
+     */
+    public abstract T update(T obj);
+
+    /**
+     * Supprime l'objet obj de la db en le sélectionnant via son id
+     * @param obj l'objet que l'on souhaite supprimer de la db
+     */
+    public abstract void delete(T obj);
 }
