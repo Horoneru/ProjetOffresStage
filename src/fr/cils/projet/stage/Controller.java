8e234efc58cf711ac99413b99423cf599ef09663
Horoneru <horonerudev@gmail.com>
2017-03-25 17:03:57 +0100

    Refactored ids, prepared styling and added ressources to the menu buttons

--- src/fr/cils/projet/stage/Controller.java
+++ src/fr/cils/projet/stage/Controller.java
@@ -1,19 +1,36 @@
 package fr.cils.projet.stage;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 
 import java.io.IOException;
-import java.net.SocketPermission;
 
 public class Controller
 {
     @FXML
     private GridPane apparenceGenerale;
+    @FXML
+    private Button btnConsulterOffre;
+    @FXML
+    private Button btnAjoutOffre;
+    @FXML
+    private Button btnAjoutEntreprise;
+    @FXML
+    private Button btnAbout;
+
+    @FXML
+    public void initialize() {
+        //impossible de rajouter correctement de graphic depuis le FXML
+        //Donc on le fait en code
+        btnConsulterOffre.setGraphic(new ImageView("file:res/consulter-offre.png"));
+        btnAjoutOffre.setGraphic(new ImageView("file:res/ajouter-offre.png"));
+        btnAjoutEntreprise.setGraphic(new ImageView("file:res/ajouter-entreprise.png"));
+        btnAbout.setGraphic(new ImageView("file:res/about.png"));
+    }
 
     public void switchApparence(MouseEvent mouseEvent) throws IOException
     {
@@ -23,15 +40,15 @@ public class Controller
 
         switch (boutonClique.getId())
         {
-            case "btn1": // Apparence 1
+            case "btnConsulterOffre": // Apparence 1
                 apparenceGenerale.add(FXMLLoader.load(getClass().getResource("ui/apparence1.fxml")),1,0);
                 break;
 
-            case "btn2":
+            case "btnAjoutOffre":
                 apparenceGenerale.add(FXMLLoader.load(getClass().getResource("ui/apparence2.fxml")),1,0);
                 break;
 
-            case "btn3":
+            case "btnAjoutEntreprise":
                 apparenceGenerale.add(FXMLLoader.load(getClass().getResource("ui/apparence3.fxml")),1,0);
                 break;
 
